08a07fba184b678773daa5492d41a89a84985242
Aubri Seebauer <bananacreature@gmail.com>
2023-02-24 20:33:49 -0500

    Gradle Changes & Config Update

--- src/main/java/com/aubrithehuman/amicore/config/AMIConfig.java
+++ src/main/java/com/aubrithehuman/amicore/config/AMIConfig.java
@@ -9,11 +9,21 @@ public final class AMIConfig {
 	public static final ForgeConfigSpec SPEC;
 	
 	public static final ForgeConfigSpec.ConfigValue<Boolean> doMalum; 
+	public static final ForgeConfigSpec.ConfigValue<Boolean> doMalumAltarFix; 
+	public static final ForgeConfigSpec.ConfigValue<Boolean> doCreativeTabs; 
+	public static final ForgeConfigSpec.ConfigValue<Boolean> doMalumStability; 
+	public static final ForgeConfigSpec.ConfigValue<Boolean> doComboCraftTweaks; 
+	public static final ForgeConfigSpec.ConfigValue<Boolean> doCOmboCraftStability; 
 	
 	static {
 		BUILDER.push("Config for AMICore");
-		
+		 
 		doMalum = BUILDER.comment("Toggle malum spirit additions. Default: true").define("Do Malum Additions", true);
+		doMalumAltarFix = BUILDER.comment("Toggle malum spirit altar fixes. Patches out duplication issue, and empty inv crash. Default: true").define("Do Spirit Altar Patches", true);
+		doCreativeTabs = BUILDER.comment("Toggle amicore creative tabs. Default: true").define("Do Creative Tabs", true);
+		doMalumStability = BUILDER.comment("Toggle stability mechanic for malum spirit altar. Default: true").define("Do Spirit Altar Stability", true);
+		doComboCraftTweaks = BUILDER.comment("Toggle stack size increase for extended crafting combination crafting recipes. Default: true").define("Do Combo Crafting Tweaks", true);
+		doCOmboCraftStability = BUILDER.comment("Toggle stability mechanic for combiantion crafting recipes. Default: true").define("Do Combo Stability", true);
 		
 		BUILDER.pop();
 		SPEC = BUILDER.build();
